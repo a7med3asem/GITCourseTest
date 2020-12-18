ba244330e460419b3490bf4f69ce4b3247bdd4a0
Ahmed Galal <ahmedgalal10895@gmail.com>
2020-12-18 16:07:23 +0200

    try sub method

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import java.util.*;
+
 public class Main{
 	
 	public static void main(String[] args){
 		System.out.println("Hello");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your first number : ");
+		int firstNum = input.nextInt();
+		
+		System.out.print("Enter your Second number : ");
+		int secondNum = input.nextInt();
+		
+		int subResult = Galal.sub(firstNum, secondNum);
+		System.out.println("Sub result is " + subResult);
 	}
 }
